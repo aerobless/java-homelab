a88ad694884447983ad2292660ababc9a16508bf
Theo Winter <theo.winter@gmail.com>
2023-05-21 13:15:23 +0200

    Adjusted module setup

--- application/src/main/java/com/sixtymeters/homelab/JavaHomelabApplication.java
+++ application/src/main/java/com/sixtymeters/homelab/JavaHomelabApplication.java
@@ -1,4 +1,4 @@
-package com.sixtymeters.javahomelab;
+package com.sixtymeters.homelab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
