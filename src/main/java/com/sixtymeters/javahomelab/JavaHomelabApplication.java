ee43a3a62ab11fde3886bc5b04ca96a6da2e87d9
Theo Winter <theo.winter@gmail.com>
2023-05-21 11:06:30 +0200

    Initial commit with spring boot backend

---
+++ src/main/java/com/sixtymeters/javahomelab/JavaHomelabApplication.java
@@ -0,0 +1,13 @@
+package com.sixtymeters.javahomelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHomelabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHomelabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sixtymeters/javahomelab/JavaHomelabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixtymeters.javahomelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHomelabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
